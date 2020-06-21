375ac2a3796d2deb5149a22f1824e60e9b8e7a8d
Franky <franky.18@ichat.sp.edu.sg>
2020-06-22 02:26:22 +0800

    Task 4

---
+++ Task 4/Task 4/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Task_4.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Task 4/Task 4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Task_4.Startup))]
+namespace Task_4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
